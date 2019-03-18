fac8abb96c242f1ea242399646e8f169f428e399
Parker Higgins <parker@parkerhiggins.net>
2019-03-17 21:27:12 -0400

    refactor with new modular structure

--- xword_dl.py
+++ xword_dl.py
@@ -5,17 +5,15 @@ import base64
 import json
 import puz
 import requests
+import urllib
+
+from bs4 import BeautifulSoup
 
 from html2text import html2text
 from unidecode import unidecode
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='URL of puzzle to download')
-    args = parser.parse_args()
-    url = args.url
-
-    res = requests.get(url, verify=False)
+def get_amuse_puzzle(url, output):
+    res = requests.get(url)
 
     rawc = next((line.strip() for line in res.text.splitlines()
                     if 'window.rawc' in line), None)
@@ -66,15 +64,81 @@ def main():
 
     p.clues.extend(normalized_clues)
 
-    filename = input("And what shall we call it? ")
+    p.save(output)
+
+    print("Puzzle downloaded and saved as {}".format(output))
+
+
+def get_newyorker_puzzle(url, output):
+    puzzle_res = requests.get(url)
+    puzzle_soup = BeautifulSoup(puzzle_res.text, "html.parser")
+
+    amuse_url = puzzle_soup.findAll('iframe', attrs={'id':'crossword'})[0]['data-src']
+
+    if output:
+        output = output if output.endswith('.puz') else ''.join([output, '.puz'])
+    else:
+        path = urllib.parse.urlsplit(url).path
+        date_frags = path.split('/')[-3:]
+        date_mash = ''.join(date_frags)
+        output = ''.join(['tny', date_mash, '.puz'])
+
+    get_amuse_puzzle(amuse_url, output)
+
+def get_latest_newyorker_puzzle(output=None):
+    index_url = "https://www.newyorker.com/crossword/puzzles-dept"
+    index_res = requests.get(index_url)
+
+    index_soup = BeautifulSoup(index_res.text, "html.parser")
+
+    latest_fragment = [a for a in index_soup.findAll('a') if a.find('h4')][0]['href']
+
+    latest_absolute = urllib.parse.urljoin('https://www.newyorker.com',
+                                            latest_fragment)
+
+    get_newyorker_puzzle(url=latest_absolute, output=output)
+
+
+def main():
     
-    if not filename:
-        filename = 'output.puz'
-    elif filename[-4:] != '.puz':
-        filename += '.puz'
+    parser = argparse.ArgumentParser()
 
-    p.save(filename)
+    extractor_parent = argparse.ArgumentParser(add_help=False)
+    date_selector = extractor_parent.add_mutually_exclusive_group()
+    date_selector.add_argument('-l', '--latest',
+                            help="""
+                                select most recent available puzzle
+                                (this is the default behavior)""",
+                            action='store_true',
+                            default=True)
+    date_selector.add_argument('-d', '--date',
+                            help='a specific puzzle date to select')
+    date_selector.add_argument('-u', '--url',
+                            help='a specific puzzle URL to downlod')
+    extractor_parent.add_argument('-o', '--output',
+                            help="""
+                            the filename for the saved puzzle
+                            (if not provided, a default value will be used)""",
+                            default=None)
 
+    subparsers = parser.add_subparsers(title='sites',
+                            description='supported puzzle sources',
+                            dest='subparser_name')
+
+    newyorker_parser = subparsers.add_parser('tny',
+                            aliases=['newyorker', 'nyer'],
+                            parents=[extractor_parent],
+                            help="download a New Yorker puzzle")
+
+    # parser.add_argument('--url', help='URL of puzzle to download')
+
+    args = parser.parse_args()
+
+    if args.subparser_name == 'tny':
+        if args.date or args.url:
+            print("haven't yet implemented specific puzzle selection")
+        elif args.latest:
+            get_latest_newyorker_puzzle(output=args.output)
 
 if __name__ == '__main__':
     main()
