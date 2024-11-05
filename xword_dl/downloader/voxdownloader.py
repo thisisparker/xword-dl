04941106bbdcb6a8f338594212b50da3aca4489b
Go1den <gooneden@gmail.com>
2024-11-05 09:59:09 -0600

    Fixed suffix list to account for more oddities

--- xword_dl/downloader/voxdownloader.py
+++ xword_dl/downloader/voxdownloader.py
@@ -20,11 +20,11 @@ class VoxDownloader(AmuseLabsDownloader):
     def find_by_date(self, dt):
         url_formatted_date = dt.strftime('%Y%m%d')
         authors = ['', 'PB', 'AP', 'WN', 'AOK', 'JG', 'AJR']  # The author varies by day, and their initials may or may not be present as a prefix
-        suffixes = ['1000', '1100', '1000%20(1)']  # The suffix is always one of these. I can't determine the pattern.
+        suffixes = ['_1000', '_1100', '', '_1000%20(1)']  # The suffix is always one of these. I can't determine the pattern.
         self.get_and_add_picker_token()
         candidate_urls = []
         for suffix in suffixes:  # On average, it will be faster to search by the most frequent suffixes first
             for author in authors:
-                self.id = author + 'vox_' + url_formatted_date + '_' + suffix
+                self.id = author + 'vox_' + url_formatted_date + suffix
                 candidate_urls.append(self.find_puzzle_url_from_id(self.id))
         return candidate_urls
