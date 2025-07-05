c55de4259ec8af71b545d79b221a938bda8f5e5a
Parker Higgins <parker@parkerhiggins.net>
2025-07-05 11:15:24 -0400

    amuse, latimes: more graceful failures for unusual puzzle ids (#248)
    * latimes, amuse: more graceful failure modes for unconventional naming
    * add title-based date check, evaluated only after the id-based one

--- xword_dl/downloader/latimesdownloader.py
+++ xword_dl/downloader/latimesdownloader.py
@@ -1,5 +1,7 @@
 import datetime
 
+import dateparser
+
 from .amuselabsdownloader import AmuseLabsDownloader
 
 
@@ -18,7 +20,15 @@ class LATimesDownloader(AmuseLabsDownloader):
 
     def guess_date_from_id(self, puzzle_id):
         date_string = "".join([char for char in puzzle_id if char.isdigit()])
-        self.date = datetime.datetime.strptime(date_string, "%y%m%d")
+        # Historically the Amuse ID has very consistently been tca_yyyymmdd
+        # but if it's not (as on 2025-07-05) this breaks without the try/except
+        try:
+            self.date = datetime.datetime.strptime(date_string, "%y%m%d")
+        except ValueError:
+            pass
+
+    def guess_date_from_puzzle_title(self, title):
+        self.date = dateparser.parse(title.split(",", maxsplit=1)[-1].strip())
 
     def find_by_date(self, dt):
         url_formatted_date = dt.strftime("%y%m%d")
@@ -29,10 +39,18 @@ class LATimesDownloader(AmuseLabsDownloader):
         return self.find_puzzle_url_from_id(self.id)
 
     def pick_filename(self, puzzle, **kwargs):
+        if not self.date and self.id:
+            self.guess_date_from_id(self.id)
+
+        if not self.date and puzzle.title:
+            self.guess_date_from_puzzle_title(puzzle.title)
+
         split_on_dashes = puzzle.title.split(" - ")
         if len(split_on_dashes) > 1:
             title = split_on_dashes[-1].strip()
-        else:
+        elif self.date:
             title = ""
+        else:
+            title = puzzle.title
 
         return super().pick_filename(puzzle, title=title, **kwargs)
