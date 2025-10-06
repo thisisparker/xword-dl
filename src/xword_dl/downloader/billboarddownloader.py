e1604689cb77a5940d2472d61a56d977d9279ca7
Parker Higgins <parker@parkerhiggins.net>
2025-10-06 13:34:01 -0400

    billboard: some title tweaks for cleaner output

--- src/xword_dl/downloader/billboarddownloader.py
+++ src/xword_dl/downloader/billboarddownloader.py
@@ -20,10 +20,20 @@ class BillboardDownloader(AmuseLabsDownloader):
 
         return url
 
+    def parse_xword(self, xw_data):
+        # Billboard puzzles are typically untitled, and some have the title set to '-'
+        # so let's clear that out so it's "truly" untitled after parsing
+        # If we see this in other outlets it might be worth promoting to the general Amuse parser
+        puzzle = super().parse_xword(xw_data)
+        if puzzle.title == "-":
+            puzzle.title = ""
+
+        return puzzle
+
     def pick_filename(self, puzzle, **kwargs):
         explicit_title = puzzle.title
 
         if not explicit_title:
-            puzzle.title = f"Billboard Crossword: {self.date: %b %d, %Y}"
+            puzzle.title = f"Billboard Crossword: {self.date:%b {self.date.day}, %Y}"
 
         return super().pick_filename(puzzle, title=explicit_title, **kwargs)
